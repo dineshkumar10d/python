424d700dc4d6545f1114bb5cdca9c6fe5130bc3b
Dineshkumar <dineshkumar.p@10decoders.in>
2021-08-16 16:53:08 +0530

    initial

---
+++ crud.py
@@ -0,0 +1,2 @@
+print("*********STUDENT MANAGEMENT SYSTEM*********")
+print("Select Any One Of The Following Option \n 1. CREATE STUDENT \n 2. READ STUDENT \n 3. UPDATE STUDENT \n 4. DELETE STUDENT \n 5. EXIT")
